3972a1d4c532cf97928e21e8d5e0abc6797e4c93
Pavel <paveln0366@gmail.com>
2021-01-09 17:31:34 +0300

    Add navigation view

--- app/src/main/java/com/epicburger/epicburgerapp/MainActivity.java
+++ app/src/main/java/com/epicburger/epicburgerapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.epicburger.epicburgerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Add burgers fragment to main activity
+        Fragment fragment = new BurgersFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
     }
 }
