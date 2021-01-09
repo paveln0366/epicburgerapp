97f1489dfb121b35a2202f9eacdc3e3f212fe7fd
Pavel <paveln0366@gmail.com>
2021-01-09 13:36:23 +0300

    Create menu fragments

---
+++ app/src/main/java/com/epicburger/epicburgerapp/SushiFragment.java
@@ -0,0 +1,18 @@
+package com.epicburger.epicburgerapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SushiFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sushi, container, false);
+    }
+}
