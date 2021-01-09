e3287cff81a72ab62c6aff035764806197b5bf70
Pavel <paveln0366@gmail.com>
2021-01-09 15:51:11 +0300

    Rename menu fragments

--- app/src/main/java/com/epicburger/epicburgerapp/DessertsFragment.java
+++ app/src/main/java/com/epicburger/epicburgerapp/DessertsFragment.java
@@ -8,11 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SushiFragment extends Fragment {
+public class DessertsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sushi, container, false);
+        return inflater.inflate(R.layout.fragment_desserts, container, false);
     }
 }
--- app/src/main/java/com/epicburger/epicburgerapp/RollsFragment.java
+++ app/src/main/java/com/epicburger/epicburgerapp/RollsFragment.java
@@ -8,11 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class PizzasFragment extends Fragment {
+public class RollsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pizzas, container, false);
+        return inflater.inflate(R.layout.fragment_rolls, container, false);
     }
 }
