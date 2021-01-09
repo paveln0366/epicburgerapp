930860981ad7ea8ae7f3ff2d6d0fcf326c4daf7a
Pavel <paveln0366@gmail.com>
2021-01-09 14:53:02 +0300

    Add HelpActivity and FeedbackActivity

---
+++ app/src/main/java/com/epicburger/epicburgerapp/HelpActivity.java
@@ -0,0 +1,19 @@
+package com.epicburger.epicburgerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        // Add toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
