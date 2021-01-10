de031809d327f664d0e0961e4ba8b787e19759b8
Pavel <paveln0366@gmail.com>
2021-01-10 14:24:44 +0300

    Add open navigation view button

--- app/src/main/java/com/epicburger/epicburgerapp/MainActivity.java
+++ app/src/main/java/com/epicburger/epicburgerapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.epicburger.epicburgerapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -17,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         // Make toolbar support action bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // create open navigation view button
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         // Add burgers fragment to main activity
         Fragment fragment = new BurgersFragment();
